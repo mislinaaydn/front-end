f8a31953f1c511cf9d977f188e59c20a9206251a
mislinaaydn <mislinaaydn26305@gmail.com>
2021-03-16 00:24:03 +0300

    second commit :)

---
+++ kamp-frontend/src/app/services/product.service.ts
@@ -0,0 +1,28 @@
+  
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { listResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'https://localhost:44334/api/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<listResponseModel<Product>> {
+   let newPath =this.apiUrl + "products/getall"
+    return this.httpClient.get<listResponseModel<Product>>(newPath);
+  
+  }
+  getProductsByCategory(categoryId:number):Observable<listResponseModel<Product>> {
+    let newPath =this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<listResponseModel<Product>>(newPath);
+  }
+}
+
+
